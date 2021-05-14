6020ef9e66b481001a94bdae190bf66700aa86be
Pablo Garrido <pablogs9@gmail.com>
2021-05-14 12:51:13 +0200

    Static type support initial
    Signed-off-by: Your Name <you@example.com>
    
    Default to OFF
    
    Signed-off-by: Your Name <you@example.com>
    
    Linter
    
    Signed-off-by: Your Name <you@example.com>
    
    Fix
    
    Signed-off-by: Your Name <you@example.com>
    
    Refactor
    
    Reafactor
    
    Signed-off-by: Your Name <you@example.com>
    
    Lint
    
    Signed-off-by: Your Name <you@example.com>

--- rosidl_typesupport_c/cmake/rosidl_typesupport_c_generate_interfaces.cmake
+++ rosidl_typesupport_c/cmake/rosidl_typesupport_c_generate_interfaces.cmake
@@ -129,8 +129,10 @@ if(NOT typesupports MATCHES ";")
     ${rosidl_generate_interfaces_TARGET}__${typesupports})
 else()
   if("${rosidl_typesupport_c_LIBRARY_TYPE}" STREQUAL "STATIC")
-    message(FATAL_ERROR "Multiple typesupports [${typesupports}] but static "
-      "linking was requested")
+  target_compile_definitions(${rosidl_generate_interfaces_TARGET}${_target_suffix}
+    PRIVATE
+    ROSIDL_TYPESUPPORT_STATIC_TYPESUPPORT
+  )
   endif()
 endif()
 
@@ -154,6 +156,7 @@ add_dependencies(
 add_dependencies(
   ${rosidl_generate_interfaces_TARGET}${_target_suffix}
   ${rosidl_generate_interfaces_TARGET}__rosidl_generator_c
+  ${rosidl_generate_interfaces_TARGET}__rosidl_typesupport_c
 )
 
 if(NOT rosidl_generate_interfaces_SKIP_INSTALL)
